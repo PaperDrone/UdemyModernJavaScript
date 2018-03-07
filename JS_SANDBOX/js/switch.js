d4b8f0731921f8c9b8025da7363c97c47a4d7fab
JieHan <jiehan.develop@gmail.com>
2018-03-08 02:01:48 +0800

    switch

---
+++ JS_SANDBOX/js/switch.js
@@ -0,0 +1,13 @@
+const color = 'red';
+
+switch (color) {
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log("Not red or blue");
+    break;
+}
