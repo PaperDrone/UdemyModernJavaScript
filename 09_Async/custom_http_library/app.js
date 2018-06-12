d5b80151cb27e10926b468b80c6133021e4a05e2
JieHan <jiehan.develop@gmail.com>
2018-06-13 00:27:51 +0800

    custom http library

---
+++ 09_Async/custom_http_library/app.js
@@ -0,0 +1,59 @@
+
+const http = new easyHTTP;
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts',
+//   function (error, posts) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(posts);
+//     }
+//   });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1',
+//   function (error, posts) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(posts);
+//     }
+//   });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// // Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data,
+//   function (err, post) {
+//     if (err) {
+//       console.log(error);
+//     } else {
+//       console.log(post);
+//     }
+//   });
+
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data,
+//   function (err, post) {
+//     if (err) {
+//       console.log(error);
+//     } else {
+//       console.log(post);
+//     }
+//   });
+
+// Delete Posts
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+  function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response);
+    }
+  });
